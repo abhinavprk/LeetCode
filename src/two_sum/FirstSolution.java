9475da07f98fe3be587c59af5c099ab63f24707e
Abhinav Prakash <email@abhinav-prakash.de>
2024-02-06 15:50:28 +0100

    TwoSum-HashMap Solution

--- src/two_sum/FirstSolution.java
+++ src/two_sum/FirstSolution.java
@@ -13,12 +13,21 @@ public class FirstSolution {
 
     }
 
+    /**
+     *
+     * @param nums array for the search
+     * @param target the addition of the numbers
+     * @return An int array containing the indexes of the values that add to target
+     *
+     * Time Complexity: O(n)
+     */
+
     public static int[] twoSum(int[] nums, int target) {
         int [] result = new int[2]; // The resulting array can contain only two elements
         checkSum:
-        for(int inputArrayIndex = 0; inputArrayIndex < nums.length - 1 ; inputArrayIndex++){
+        for(int inputArrayIndex = 0; inputArrayIndex < nums.length - 1 ; inputArrayIndex++){ //O(n)
             int firstNumber = nums[inputArrayIndex];
-            for(int compareArrayIndex = inputArrayIndex + 1; compareArrayIndex < nums.length; compareArrayIndex++){
+            for(int compareArrayIndex = inputArrayIndex + 1; compareArrayIndex < nums.length; compareArrayIndex++){ //O(n)
                 int secondNumber = nums[compareArrayIndex];
                 if((target - secondNumber == firstNumber)) {
                     //numbers found, add the indexes in the result array
---
+++ src/two_sum/SecondSolution.java
@@ -0,0 +1,39 @@
+package two_sum;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Use hashMap to store values and compare later on with it
+ *
+ */
+public class SecondSolution {
+
+    public static void main(String[] args) {
+
+        int [] nums = new int[]{2, 5, 8, 3};
+        int target = 7;
+
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int [] result = new int[2]; // The resulting array can contain only two elements
+        Map<Integer, Integer> complementMap = new HashMap<>();
+        for(int inputIndex = 0; inputIndex < nums.length ; inputIndex++){
+            int complement = target - nums[inputIndex];
+            if(complementMap.containsKey(complement)){
+                result[0] = complementMap.get(complement); //complement will always be smaller
+                result[1] = inputIndex;
+                break;
+            } else {
+                complementMap.put(nums[inputIndex], inputIndex);
+
+            }
+
+        }
+        return result;
+    }
+}
