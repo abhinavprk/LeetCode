4c0d7283f47fb52e1154d57c2699f9dcd5b1c064
Abhinav Prakash <email@abhinav-prakash.de>
2024-02-18 16:49:09 +0100

    leetcode solution

---
+++ src/lc2402MeetingRoomIII/Solution1.java
@@ -0,0 +1,70 @@
+package lc2402MeetingRoomIII;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        int noOfRooms = 2;
+        int [][] meetings = {{0,10},{1,5},{2,7},{3,4}};
+        System.out.println(mostBooked(noOfRooms, meetings));
+    }
+
+    private static int mostBooked(int noOfRooms, int[][] meetings) {
+        //Sort the meetings with startTime
+        Arrays.sort(meetings, (a, b) -> a[0] != b[0] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1]));
+        long [] roomAvailabilityTime = new long [noOfRooms];
+        int [] meetingCount = new int [noOfRooms];
+        for (int[] meeting : meetings) {
+            //Iterate through the meetings
+            int startTime = meeting[0];
+            int endTime = meeting[1];
+            boolean meetingAssigned = false; //If the meeting is assigned or not
+            for (int room = 0; room < roomAvailabilityTime.length; room++) {
+                //Iterate through the rooms for their availability
+                long availableFrom = roomAvailabilityTime[room];
+                if (availableFrom <= startTime) {
+                    //This room is available to be occupied by this meeting
+                    //Update the availability time
+                    roomAvailabilityTime[room] = endTime;
+                    //Increase the meeting count
+                    meetingCount[room]++;
+                    meetingAssigned = true;
+                    break;
+                }
+            }
+            //If meeting is not assigned that means no room is free
+            //Find the next available room with the lowest index
+            if (!meetingAssigned) {
+                int nextAvailableRoom = -1;
+                long nextAvailabilityFrom = Long.MAX_VALUE;
+                for (int room = 0; room < roomAvailabilityTime.length; room++) {
+                    //Iterate through the rooms for their availability
+                    long availableFrom = roomAvailabilityTime[room];
+                    if (availableFrom < nextAvailabilityFrom) {
+                        //This will be the room with the lower index even in the case of two rooms have the same
+                        //availability time
+                        nextAvailableRoom = room;
+                        nextAvailabilityFrom = availableFrom;
+                    }
+                }
+                meetingCount[nextAvailableRoom]++;
+                roomAvailabilityTime[nextAvailableRoom] = nextAvailabilityFrom + (endTime - startTime);
+            }
+        }
+        //Find the meeting room with maximum meetings
+        int maximumCount = -1;
+        int lowestIndexForMaximumCount = Integer.MAX_VALUE;
+        for (int room = 0; room < meetingCount.length; room++) {
+            int count = meetingCount[room];
+            //This will choose the meeting room with the highest meetings but that is of lowest index
+            if(count > maximumCount){
+                maximumCount = count;
+                lowestIndexForMaximumCount = room;
+            }
+        }
+        return lowestIndexForMaximumCount;
+    }
+}
---
+++ src/lc2402MeetingRoomIII/Solution2.java
@@ -0,0 +1,64 @@
+package lc2402MeetingRoomIII;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Solution2 {
+
+    public static void main(String[] args) {
+        int noOfRooms = 2;
+        int [][] meetings = {{0,10},{1,5},{2,7},{3,4}};
+        System.out.println(mostBooked(noOfRooms, meetings));
+    }
+
+    private static int mostBooked(int n, int[][] meetings) {
+        var meetingCount = new int[n];
+        //The usedRooms priorityQue contains long[] with two values each, end of the meeting, and the room.
+        // It is ordered by first by the end time and then by the room index.
+        var usedRooms = new PriorityQueue<long[]>((a, b) -> a[0] != b[0] ? Long.compare(a[0], b[0]) : Long.compare(a[1], b[1]));
+        //The unusedRooms priorityQueue contains the room indexes
+        var unusedRooms = new PriorityQueue<Integer>();
+        //Prepare the unusedRooms
+        for (int i = 0; i < n; i++) {
+            unusedRooms.offer(i);
+        }
+        //sort the meetings first by start time and then by end times
+        Arrays.sort(meetings, (a, b) -> a[0] != b[0] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1]));
+        //Traverse the meetings
+        for (int[] meeting : meetings) {
+            int start = meeting[0];
+            int end = meeting[1];
+            //fill up unusedRooms for current meeting. All the rooms for which end is before the start of the current meeting
+            while (!usedRooms.isEmpty() && usedRooms.peek()[0] <= start) {
+                int room = (int) usedRooms.poll()[1];
+                unusedRooms.offer(room);
+            }
+
+            if (!unusedRooms.isEmpty()) {
+                //If unused rooms are available
+                int room = unusedRooms.poll();
+                usedRooms.offer(new long[]{end, room});
+                meetingCount[room]++;
+            } else if(!usedRooms.isEmpty()){
+                //else if used rooms is not empty, pick the room for which the meeting is ending first and update for the current meeting
+                long roomAvailabilityTime = usedRooms.peek()[0];
+                int room = (int) usedRooms.poll()[1];
+                usedRooms.offer(new long[]{roomAvailabilityTime + end - start, room});
+                meetingCount[room]++;
+            }
+        }
+
+        int maxMeetingCount = 0;
+        int maxMeetingCountRoom = 0;
+        //find the room with maximum meetings
+        for (int i = 0; i < n; i++) {
+            if (meetingCount[i] > maxMeetingCount) {
+                maxMeetingCount = meetingCount[i];
+                maxMeetingCountRoom = i;
+            }
+        }
+
+        return maxMeetingCountRoom;
+    }
+}
