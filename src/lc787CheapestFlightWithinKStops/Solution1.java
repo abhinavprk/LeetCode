11a85b47846b561b2dbf37d81f374a588fce1e7a
Abhinav Prakash <email@abhinav-prakash.de>
2024-02-23 14:53:56 +0100

    Lc787 (#36)
    * draft
    
    * draft

--- src/lc787CheapestFlightWithinKStops/Solution1.java
+++ src/lc787CheapestFlightWithinKStops/Solution1.java
@@ -1,2 +1,20 @@
-package lc787CheapestFlightWithinKStops;public class Solution1 {
+package lc787CheapestFlightWithinKStops;
+
+
+public class Solution1 {
+   //TODO
+    public static void main(String[] args) {
+
+        int n = 4;
+        int [] [] flights = {{0,1,100}, {1,2,100},{2,0,100},{1,3,600},{2,3,200}};
+        int src = 0;
+        int dst = 3;
+        int k = 1;
+
+    }
+
+    private static int findCheapestFlight(int n, int[][] flights, int src, int dst, int k){
+
+        return 0;
+    }
 }
