b397c6c71d4041473a9afe53571a8f129e69dbe5
Abhinav Prakash <email@abhinav-prakash.de>
2024-03-02 15:19:34 +0530

    Onlogn

---
+++ src/lc41FirstMissingPositive/Solution2.java
@@ -0,0 +1,47 @@
+package lc41FirstMissingPositive;
+
+import java.util.Arrays;
+
+/*
+O(n) solution
+ */
+public class Solution2 {
+
+    public static void main(String[] args) {
+        System.out.println(firstMissingPositive(new int[]{1,2,0}));
+        System.out.println(firstMissingPositive(new int[]{3,4,-1,1}));
+        System.out.println(firstMissingPositive(new int[]{7,8,9,11,12}));
+
+    }
+
+    private static int firstMissingPositive(int[] nums) {
+        int smallest = Integer.MAX_VALUE;
+        int largest = Integer.MIN_VALUE;
+        int missingVal =0;
+        int countPositive = 0;
+        for (int curr : nums) {
+            if (curr > 0) {
+                countPositive++;
+                if (curr < smallest) {
+                    smallest = curr;
+                }
+                if (curr > largest) {
+                    largest = curr;
+                }
+            }
+        }
+        Arrays.sort(nums);
+        if (smallest > 1) {
+            missingVal = 1;
+        } else {
+            for(int i = 2; i<= countPositive+1; i++){
+                int res = Arrays.binarySearch(nums, i);
+                if(res < 0){
+                    missingVal = i;
+                    break;
+                }
+            }
+        }
+        return missingVal;
+    }
+}
