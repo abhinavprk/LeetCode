c3696753548dfcbadc912113ab582e3c266171cf
Abhinav Prakash <email@abhinav-prakash.de>
2024-03-26 22:17:49 +0530

    test case

--- src/lc442FindAllDuplicatesInArray/Solution1.java
+++ src/lc442FindAllDuplicatesInArray/Solution1.java
@@ -8,6 +8,7 @@ public class Solution1 {
 
     public static void main(String[] args) {
         System.out.println(findDuplicates(new int[]{4,3,2,7,8,2,3,1}));
+        System.out.println(findDuplicates(new int[]{}));
     }
     private static List<Integer> findDuplicates(int[] nums) {
         List<Integer> ans = new ArrayList<>();
