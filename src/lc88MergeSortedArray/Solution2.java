50b84a86f03a3d74c63886944a33a7e70551ff90
Abhinav Prakash <email@abhinav-prakash.de>
2024-04-30 00:05:19 +0530

    brut force

---
+++ src/lc88MergeSortedArray/Solution2.java
@@ -0,0 +1,17 @@
+package lc88MergeSortedArray;
+
+import java.util.Arrays;
+
+public class Solution2 {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(merge(new int[]{1,2,3,0,0,0}, 3, new int[]{2,5,6}, 3)));
+        System.out.println(Arrays.toString(merge(new int[]{1}, 1, new int[]{}, 0)));
+
+    }
+
+    private static int[] merge(int[] nums1, int m, int[] nums2, int n) {
+
+        return nums1;
+    }
+}
