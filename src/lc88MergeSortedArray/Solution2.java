1a428a7839cadfb6a1af8e4af50af64bceabf217
Abhinav Prakash <email@abhinav-prakash.de>
2024-05-05 19:15:13 +0530

    Lc493 (#115)
    * commit
    
    * submission

--- src/lc88MergeSortedArray/Solution2.java
+++ src/lc88MergeSortedArray/Solution2.java
@@ -6,7 +6,7 @@ public class Solution2 {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(merge(new int[]{2, 5, 6, 0, 0, 0}, 3, new int[]{1, 2, 3}, 3)));
-        System.out.println(Arrays.toString(merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3)))
+        System.out.println(Arrays.toString(merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3)));
         System.out.println(Arrays.toString(merge(new int[]{1}, 1, new int[]{}, 0)));
 
     }
