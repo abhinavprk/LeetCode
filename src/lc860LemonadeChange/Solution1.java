41372b89b7f786acdbf2387b2fad31017897075f
Abhinav Prakash <email@abhinav-prakash.de>
2025-01-29 14:43:14 +0100

    improved lemonade change

--- src/lc860LemonadeChange/Solution1.java
+++ src/lc860LemonadeChange/Solution1.java
@@ -5,39 +5,33 @@ public class Solution1 {
     public static void main(String[] args) {
         System.out.println(lemonadeChange(new int[]{5, 5, 5, 10, 20}));
         System.out.println(lemonadeChange(new int[]{5, 5, 10, 10, 20}));
-        System.out.println(lemonadeChange(new int[]{5,5,10,20,5,5,5,5,5,5,5,5,5,10,5,5,20,5,20,5}));
+        System.out.println(lemonadeChange(new int[]{5, 5, 10, 20, 5, 5, 5, 5, 5, 5, 5, 5, 5, 10, 5, 5, 20, 5, 20, 5}));
     }
 
     private static boolean lemonadeChange(int[] bills) {
         int countFives = 0;
         int countTens = 0;
         for (int currentBill : bills) {
-            if(currentBill == 5){
+            if (currentBill == 5) {
                 countFives++;
             } else if (currentBill == 10) {
-                if(countFives < 1){
+                if (countFives == 0) {
                     return false;
-                } else {
-                    countFives--;
-                    countTens++;
                 }
+                countFives--;
+                countTens++;
             } else {
-                if(countTens < 1){
-                    if(countFives < 3){
-                        return false;
-                    }else {
-                        countFives = countFives - 3;
-                    }
-                }else {
-                    if(countFives < 1){
-                        return false;
-                    }else {
-                        countFives--;
-                        countTens--;
-                    }
+                //Richten Sie die Bedingungen danach aus, was Sie brauchen, nicht danach, was Sie nicht brauchen. Wir müssen die größten Scheine zuerst ausgeben.
+                if ((countTens > 0) && (countFives > 0)) {
+                    countFives--;
+                    countTens--;
+                } else if (countFives >= 3) {
+                    countFives -= 3;
+                } else {
+                    return false;
                 }
             }
         }
-        return  true;
+        return true;
     }
 }
