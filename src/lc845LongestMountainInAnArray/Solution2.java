fd7cff4719afc76866b714ddb43c8081cd65d71b
Abhinav Prakash <email@abhinav-prakash.de>
2025-02-05 10:06:05 +0100

    longest mountain

--- src/lc845LongestMountainInAnArray/Solution2.java
+++ src/lc845LongestMountainInAnArray/Solution2.java
@@ -1,6 +1,6 @@
 package lc845LongestMountainInAnArray;
 
-public class Solution1 {
+public class Solution2 {
 
     public static void main(String[] args) {
         System.out.println(longestMountain(new int[] {2,1,4,7,3,2,5}));
@@ -12,11 +12,12 @@ public class Solution1 {
     private static int longestMountain(int[] arr){
 
         int longest = 0;
-        //Assume every index could be a peak and check for each of them if it is the peak for the longest mountain
-        for (int i = 1; i < arr.length -1 ; i++) {
-            int peak = arr[i];
-            int left = i-1;
-            int right = i + 1;
+        //Since the array length in min 3, the peak could be index 1 only
+        int peakIndex = 1;
+        while (peakIndex < arr.length - 1) {
+            int peak = arr[peakIndex];
+            int left = peakIndex-1;
+            int right = peakIndex + 1;
             //move the left boundary until left is smaller than peak and also smaller than the adjacent element to it's left
             if((arr[left] < peak) && (arr[right]<peak)) {
                 while (left > 0  && (arr[left - 1] < arr[left])) {
@@ -26,10 +27,13 @@ public class Solution1 {
                 while (right < arr.length - 1 && (arr[right + 1] < arr[right])) {
                     right++;
                 }
-                //Update the longest length based on the length of the current mountain.
+                //Update the longest based on the current length of the mountain
                 longest = Math.max(longest, right - left + 1);
             }
+            peakIndex = right;
         }
+
+
         return longest;
     }
 }
